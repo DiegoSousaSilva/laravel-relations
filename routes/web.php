23e3be3e7877d5b693e0c973067150756621704d
DiegoSousaSilva <derickbenji2@gmail.com.br>
2022-01-21 13:49:39 -0300

    Relacionamento One-to-One

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\{User, Prefence, Preference};
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,29 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/one-to-one', function(){
+   // Pega primeiro usuario $user = User::first();
+    $user = User::with('preference')->find(2);
+
+    $data = [
+         'background-color' => '#000',
+    ];
+
+    if ($user->preference) {
+        $user->preference->update($data);
+    } else {
+        //$user->preference()->create($data);
+        $preference = new Preference($data);
+        $user->preference()->save($preference);
+    }
+
+    $user->refresh();
+    var_dump($user->preference);
+
+    $user->preference->delete();
+
+    $user->refresh();
+    dd($user->preference()->get());
 });
